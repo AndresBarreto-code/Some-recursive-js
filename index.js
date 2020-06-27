dfdf2f1b069e346328b99e076cb15ec05422e8b9
Andrés Felipe Barreto Guio <baguioandres@gmail.com>
2020-06-26 21:59:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -76,17 +76,18 @@ function divisores(x,y){
     return count;
 }
 function Ackermann(m,n){
-    if(m=0){
+    console.log(m,n)
+    if(m==0){
         return n+1;
-    }else if(m > 0 && n == 0){
-        return Ackermann(m-1,n);
-    }else if(m > 0 && n > 0){
+    }else if(n == 0){
+        return Ackermann(m-1,1);
+    }else{
         return Ackermann(m-1,Ackermann(m,n-1));
     }
 }
 $(document).ready(
     ()=>{
         resultado=document.getElementById('resultado');
-        resultado.innerHTML=Ackermann(4,2);
+        resultado.innerHTML=Ackermann(2,2);
     }
 );
